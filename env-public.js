2f4345647511ecec6682214c32b0a74cbfd24424
naki357 <thetoddnakamura@gmail.com>
2016-04-03 08:43:23 -0400

    First commit of this environment variable file.

---
+++ env-public.js
@@ -0,0 +1,17 @@
+// After creating an application at apps.twitter.com, this file should be configured to match
+// the credentials for your own app.  This should then also be renamed as env.js  
+
+module.exports = {
+  getConsumerKey: function () {
+    return "[Your Twitter Consumer Key]"; 
+  },
+  getConsumerSecret: function () {
+    return "[Your Twitter Consumer Secret]"; 
+  },
+  getAccessTokenKey: function () {
+    return "[Your Twitter Access Token Key]"; 
+  },
+  getAccessTokenSecret: function () {
+    return "[Your Twitter Token Secret]"; 
+  }
+};
